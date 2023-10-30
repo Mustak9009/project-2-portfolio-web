424fe5ea77c2f1a46c3b62abe6c11febe71b32ef
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2023-10-30 10:12:07 +0530

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Full stack web Developer - Mustak',
-  description: 'Skilled Full Stack Web Developer: Proficient in front-end and back-end technologies, creating dynamic, secure, and responsive web applications. nextjs,reactjs',
-}
+  title: "Full stack web Developer - Mustak",
+  description:
+    "Skilled Full Stack Web Developer: Proficient in front-end and back-end technologies, creating dynamic, secure, and responsive web applications. nextjs,reactjs",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
