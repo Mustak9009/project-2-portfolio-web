5e02b98ca5d48ca92feee0dc140ea56082afe0b3
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2023-11-03 09:25:42 +0530

    add

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+'use client'
+import React,{useState} from "react";
 
 export default function Page() {
+  const [password,setPassword] = useState<string>('');
   return (
-    <div>
-
+    <div className="absolute inset-0 bg-white">
+      <form className="w-full  flex justify-center items-center h-screen flex-col space-y-5">
+        <h2 className="text-red-500 text-5xl font-bold">Admin</h2>
+        <input type="password" name="password" placeholder="Enter password" className="bg-transparent border border-black rounded-md px-3 py-1 w-[20rem]  outline-none" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        <button type="submit" className="bg-black text-white px-4 py-1 rounded-md">submit</button>
+      </form>
     </div>
-  )
+  );
 }
